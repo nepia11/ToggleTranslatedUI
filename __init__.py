2e641daec352cf880fadecbce535e9332ece75fd
nepia11 <kapibara19@gmail.com>
2021-04-15 23:35:26 +0900

    翻訳切り替えの代わりに言語設定を保存しておいて言語切り替えをするようにしてみた

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ import bpy
 
 bl_info = {
     "name": "Toggle Translated UI",
-    "author": "Original:Satoshi Yamasaki(yamyam), Converted to 2.83: Toudou++",
-    "version": (3, 0),
+    "author": "Original:Satoshi Yamasaki(yamyam), Converted to 2.83: Toudou++, nepia11",
+    "version": (4, 0),
     "blender": (2, 83, 0),
     "description": "Toggle International Fonts option to switch translated / non transleted UI.",
     "location": "shortcut: End key",
@@ -20,6 +20,7 @@ class OBJECT_OT_translatedUI_toggle(bpy.types.Operator):
     bl_label = "Toggle Translated UI"
 
     def execute(self, context):
+        # インターフェースの翻訳をするか否かを切り替えている
         b = bpy.context.preferences.view.use_translate_interface
         bpy.context.preferences.view.use_translate_interface = not b
         b = bpy.context.preferences.view.use_translate_tooltips
@@ -27,27 +28,60 @@ class OBJECT_OT_translatedUI_toggle(bpy.types.Operator):
         return {"FINISHED"}
 
 
+class Language_Toggle(bpy.types.Operator):
+    """ toggle language """
+
+    bl_idname = "preferences.language_toggle"
+    bl_label = "Toggle UI Language"
+
+    # 切り替える前の状態を保存しておく
+    lang = bpy.context.preferences.view.language
+    interface_flag = bpy.context.preferences.view.use_translate_interface
+    tooltips_flag = bpy.context.preferences.view.use_translate_tooltips
+    new_dataname_flag = bpy.context.preferences.view.use_translate_new_dataname
+
+    def execute(self, context):
+        # 言語切り替えをする
+        pref = bpy.context.preferences
+        if pref.view.language == self.lang:
+            pref.view.language = "en_US"
+        else:
+            pref.view.language = self.lang
+            pref.view.use_translate_interface = self.interface_flag
+            pref.view.use_translate_tooltips = self.tooltips_flag
+            pref.view.use_translate_new_dataname = self.new_dataname_flag
+        return {"FINISHED"}
+
+
 # Registration
+classes = [
+    OBJECT_OT_translatedUI_toggle,
+    Language_Toggle,
+]
+
+keymaps = []
 
 
 def register():
-    bpy.utils.register_class(OBJECT_OT_translatedUI_toggle)
+    for c in classes:
+        bpy.utils.register_class(c)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps.new(name="Window", space_type="EMPTY")
         #        kmi = km.keymap_items.new('object.translatedui_toggle', 'SPACE', 'PRESS', shift=True)
-        kmi = km.keymap_items.new("object.translatedui_toggle", "END", "PRESS")
+        # kmi = km.keymap_items.new("object.translatedui_toggle", "END", "PRESS")
+        kmi = km.keymap_items.new("preferences.language_toggle", "END", "PRESS")
+        keymaps.append((km, kmi))
 
 
 def unregister():
-    bpy.utils.unregister_class(OBJECT_OT_translatedUI_toggle)
+    for c in classes:
+        bpy.utils.unregister_class(c)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
-        km = kc.keymaps["Window"]
-        for kmi in km.keymap_items:
-            if kmi.idname == "object.translatedui_toggle":
-                km.keymap_items.remove(kmi)
-                break
+        for km, kmi in keymaps:
+            # ショートカットキーの登録解除
+            km.keymap_items.remove(kmi)
 
 
 if __name__ == "__main__":
